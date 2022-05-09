1653c4e84bb9930691dff71a0700260aace5e9f5
Serjamongus <sershtroy@yandex.ru>
2022-05-09 18:07:06 +0500

    Почти доделал ДЗ1

--- app/src/main/java/com/serjamongus/loftmoney4/AddItemActivity.java
+++ app/src/main/java/com/serjamongus/loftmoney4/AddItemActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdditemActivity extends AppCompatActivity {
+public class AddItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
+        setContentView(R.layout.activity_add_item);}
+
+
 }
--- app/src/main/java/com/serjamongus/loftmoney4/MainActivity.java
+++ app/src/main/java/com/serjamongus/loftmoney4/MainActivity.java
@@ -2,19 +2,21 @@ package com.serjamongus.loftmoney4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+Intent activity = new Intent(this, AddItemActivity.class);
+startActivity(activity);}
+
+    }
 
 
-}
-// TODO: We will work with recyclerview
-// Commit is work
 
-   // TODO: Add to list
+
 
