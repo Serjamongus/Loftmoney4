bac2dba584d97595a99168c3b6fc4369e169d682
Serjamongus <sershtroy@yandex.ru>
2022-05-17 11:36:04 +0500

    Добавил библиотеку recycler, material 1.6.0

---
+++ app/src/main/java/com/serjamongus/loftmoney4/MoneyItemsAdapter.java
@@ -0,0 +1,23 @@
+package com.serjamongus.loftmoney4;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MoneyItemsAdapter extends RecyclerView.Adapter<MoneyItemsAdapter.MoneyViewHolder> {
+    @NonNull
+    @Override
+    public MoneyItemsAdapter.MoneyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoneyItemsAdapter.MoneyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
